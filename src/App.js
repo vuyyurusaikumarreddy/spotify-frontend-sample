53b3fc5131bbfac4135e75d2b88c987184b8beb6
vuyyurusaikumarreddy <vuyyurusaikumarreddy2001@gmail.com>
2023-12-06 17:09:01 -0500

    added navigator bar and home components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Playlist from './components/Playlist';
+import Home from './components/Home';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <Playlist />
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavigationBar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/playlist" element={<Playlist />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
